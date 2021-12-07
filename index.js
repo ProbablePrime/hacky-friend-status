72721e8e3e71ccb44cce556815727460559e4cea
Richard Fox <richard.fox@ahref.co.uk>
2021-12-07 12:33:57 -0800

    just list them

--- index.js
+++ index.js
@@ -10,34 +10,13 @@ async function main() {
     const token = await login(config.get('username'),config.get('password'));
     const friends = await getFriends(token, config.get('id'));
 
-    const offlineFriends = friends
-        .filter(function(friend) {
-            return friend.userStatus.onlineStatus === 'Offline';
-        })
-        .filter(function(friend) {
-            return friend.friendUsername !== 'Neos';
-        });
-
-    const filteredFriends = offlineFriends.map(function(friend) {
+    const filteredFriends = friends.map(function(friend) {
         return {
             username: friend.friendUsername,
             id: friend.id
         }
-    }); 
-    console.log(filteredFriends.length);
-
-    for (let friend of filteredFriends) {
-        console.log('Checking ' + friend.username);
-        const status = await getStatus(friend.id);
-        friend.lastStatus = new Date(status.lastStatusChange);
-    }
-    const threeMonthsAgo = new Date();
-    threeMonthsAgo.setMonth(threeMonthsAgo.getMonth() - 3);
-
-    const vanishedFriends = filteredFriends.filter(function(friend) {
-        return friend.lastStatus < threeMonthsAgo;
     });
-    writeCSV(vanishedFriends, 'vanished.csv');
+    writeCSV(filteredFriends, 'friends.csv');
 }
 
 function writeCSV(friends, name) {
